955c72681f0dc0d93b50a35787a4e51d5b22d639
YongBum <dt10002@naver.com>
2025-05-04 23:07:55 +0900

    Querydsl 실무 활용 2

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryImplTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryImplTest.java
@@ -1,12 +1,14 @@
 package study.querydsl.repository;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import jakarta.persistence.EntityManager;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -15,33 +17,16 @@ import study.querydsl.entity.Team;
 
 @Transactional
 @SpringBootTest
-class MemberJpaRepositoryTest {
+class MemberRepositoryImplTest {
 
   @Autowired
   EntityManager em;
 
   @Autowired
-  MemberJpaRepository memberJpaRepository;
-  @Autowired
   private MemberRepository memberRepository;
 
   @Test
-  void basicTest() {
-    Member member = new Member("member1", 10);
-    memberJpaRepository.save(member);
-
-    Member findMember = memberJpaRepository.findById(member.getId()).get();
-    assertThat(findMember).isEqualTo(member);
-
-    List<Member> result1 = memberJpaRepository.findAll_Querydsl();
-    assertThat(result1).containsExactly(member);
-
-    List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
-    assertThat(result2).containsExactly(member);
-  }
-
-  @Test
-  void searchTest() {
+  void searchPageSimple() {
     Team teamA = new Team("teamA");
     Team teamB = new Team("teamB");
     em.persist(teamA);
@@ -57,12 +42,13 @@ class MemberJpaRepositoryTest {
     em.persist(member4);
 
     MemberSearchCondition condition = new MemberSearchCondition();
-    condition.setAgeGoe(35);
-    condition.setAgeLoe(40);
-    condition.setTeamName("teamB");
+    PageRequest pageRequest = PageRequest.of(0, 3);
+    Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
 
-    List<MemberTeamDto> result = memberRepository.search(condition);
-    assertThat(result).extracting("username").containsExactly("member4");
+    assertThat(result.getSize()).isEqualTo(3);
+    assertThat(result.getContent()).extracting("username")
+        .containsExactly("member1", "member2", "member3");
+
   }
 
 }
