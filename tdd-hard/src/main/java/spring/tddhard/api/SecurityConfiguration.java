391d1eeb00dad3e36766523ff1ed39e88be56f10
Yongbum Kim <dt10002@naver.com>
2025-06-25 14:55:19 +0900

    Study: Spring 빈 등록

---
+++ tdd-hard/src/main/java/spring/tddhard/api/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package spring.tddhard.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration {
+
+  @Bean
+  Pbkdf2PasswordEncoder passwordEncoder() {
+    return Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+  }
+
+}
