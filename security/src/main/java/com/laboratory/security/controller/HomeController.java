45e4bc2f878f336002d238ed6468562521785808
Yongbum Kim <dt10002@naver.com>
2025-12-26 00:16:29 +0900

    feat: 기본 Controller 구현 및 Security 인증 테스트

---
+++ security/src/main/java/com/laboratory/security/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.laboratory.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping(value = {"", "/"})
+  public String index() {
+    return "Hello Spring Security";
+  }
+
+}
