6004907b8cf31b593281de6ee5506652e35118a4
Yongbum Kim <dt10002@naver.com>
2025-12-28 01:13:32 +0900

    feat: SecurityConfig 설정 강화
    - PasswordEncoder Bean 객체로 지정했습니다.
    - URL 엔드포인트 형식으로 인가 규칙을 지정했습니다.
    - 폼 로그인 및 로그아웃과 관련된 설정을 구현했습니다.
    - CSRF 토큰을 비활성화했습니다.
    - H2 콘솔을 위해서 부가적인 설정을 추가했습니다.

--- security/src/main/java/com/laboratory/security/config/SecurityConfig.java
+++ security/src/main/java/com/laboratory/security/config/SecurityConfig.java
@@ -1,10 +1,16 @@
 package com.laboratory.security.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import static org.springframework.http.HttpMethod.GET;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,11 +18,46 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    // 폼 로그인 인증 활성화
+
+    // 인가 규칙 지정
+    httpSecurity.authorizeHttpRequests(
+        registry -> registry
+            .requestMatchers(toH2Console()).permitAll()
+            .requestMatchers("/signup").permitAll()
+            .requestMatchers(GET, "/login").permitAll()
+            .anyRequest().authenticated()
+    );
+
+    // 폼 로그인 인증 설정
     httpSecurity.formLogin(
-        // 기본 설정 사용
-        Customizer.withDefaults()
+        configurer -> configurer
+            .defaultSuccessUrl("/home")
+            .loginPage("/login")
+            .loginProcessingUrl("/login/processing")
+            .failureUrl("/login?fail")
+    );
+
+    // 로그아웃 설정
+    httpSecurity.logout(
+        configurer -> configurer.logoutUrl("/logout/processing")
+    );
+
+    // CSRF 비활성화
+    httpSecurity.csrf(
+        AbstractHttpConfigurer::disable
+    );
+
+    // H2 콘솔을 위한 부가 설정
+    httpSecurity.headers(
+        configurer -> configurer.frameOptions(
+            HeadersConfigurer.FrameOptionsConfig::disable
+        )
     );
 
     return httpSecurity.build();
