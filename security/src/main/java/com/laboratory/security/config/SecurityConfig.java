7a87b47e3ef6533899a7e2dca2372acacc219648
Yongbum Kim <dt10002@naver.com>
2025-12-28 16:49:41 +0900

    feat: 익명 객체를 테스트해보기 위한 코드 구현

--- security/src/main/java/com/laboratory/security/config/SecurityConfig.java
+++ security/src/main/java/com/laboratory/security/config/SecurityConfig.java
@@ -1,16 +1,10 @@
 package com.laboratory.security.config;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-import static org.springframework.http.HttpMethod.GET;
-
+import com.laboratory.security.domain.AnonymousUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,46 +12,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
-
-  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-
-    // 인가 규칙 지정
     httpSecurity.authorizeHttpRequests(
-        registry -> registry
-            .requestMatchers(toH2Console()).permitAll()
-            .requestMatchers("/signup").permitAll()
-            .requestMatchers(GET, "/login").permitAll()
-            .anyRequest().authenticated()
+        registry -> registry.anyRequest().permitAll()
     );
 
-    // 폼 로그인 인증 설정
-    httpSecurity.formLogin(
+    /* 익명 인증인 경우 인증 주체로 사용할 객체 등록 */
+    httpSecurity.anonymous(
         configurer -> configurer
-            .defaultSuccessUrl("/home")
-            .loginPage("/login")
-            .loginProcessingUrl("/login/processing")
-            .failureUrl("/login?fail")
-    );
-
-    // 로그아웃 설정
-    httpSecurity.logout(
-        configurer -> configurer.logoutUrl("/logout/processing")
-    );
-
-    // CSRF 비활성화
-    httpSecurity.csrf(
-        AbstractHttpConfigurer::disable
-    );
-
-    // H2 콘솔을 위한 부가 설정
-    httpSecurity.headers(
-        configurer -> configurer.frameOptions(
-            HeadersConfigurer.FrameOptionsConfig::disable
-        )
+            .principal(new AnonymousUser())
+            .authorities("ANONYMOUS_USER")
     );
 
     return httpSecurity.build();
