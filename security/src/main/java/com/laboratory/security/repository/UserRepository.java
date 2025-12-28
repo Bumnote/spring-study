826a7174dccb524de719b6206acb3f6c4da0a10d
Yongbum Kim <dt10002@naver.com>
2025-12-29 02:46:40 +0900

    feat: OAuth2 외부 로그인을 위한 Entity 및 Repository 구현

--- security/src/main/java/com/laboratory/security/repository/UserRepository.java
+++ security/src/main/java/com/laboratory/security/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.laboratory.security.repository;
 
 import com.laboratory.security.domain.entity.UserEntity;
+import com.laboratory.security.service.LoginType;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, String> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
+  Optional<UserEntity> findBySubjectAndLoginType(String subject, LoginType loginType);
 }
---
+++ security/src/main/java/com/laboratory/security/service/LoginType.java
@@ -0,0 +1,6 @@
+package com.laboratory.security.service;
+
+public enum LoginType {
+  GOOGLE,
+  NAVER
+}
