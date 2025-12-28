1293d5a8c1d184d09a2be1dd422a61066c0a93e1
Yongbum Kim <dt10002@naver.com>
2025-12-29 01:20:22 +0900

    feat: csrf & cors 기능 테스트를 위한 Security 설정 구현

--- security/src/main/java/com/laboratory/security/config/SecurityConfig.java
+++ security/src/main/java/com/laboratory/security/config/SecurityConfig.java
@@ -1,26 +1,36 @@
 package com.laboratory.security.config;
 
-import org.springframework.context.ApplicationEventPublisher;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationEventPublisher;
-import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
   @Bean
-  public AuthorizationEventPublisher authorizationEventPublisher(
-      ApplicationEventPublisher applicationEventPublisher
-  ) {
-    return new SpringAuthorizationEventPublisher(applicationEventPublisher);
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    /* [1] 허용 출처 등록 */
+    configuration.setAllowedOrigins(List.of("http://allowed-origin.com:8080 "));
+
+    /* [2] 허용 메서드 등록 */
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    /* [3] CORS 허용하는 경로 등록 */
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
@@ -28,11 +38,19 @@ public class SecurityConfig {
 
     httpSecurity.authorizeHttpRequests(
         registry -> registry
-            .requestMatchers("/v1/**").denyAll()
             .anyRequest().authenticated()
     );
 
+    httpSecurity.cors(
+        configurer -> configurer
+            .configurationSource(corsConfigurationSource())
+    );
+
     httpSecurity.httpBasic(
+        Customizer.withDefaults()
+    );
+
+    httpSecurity.csrf(
         Customizer.withDefaults()
     );
 
