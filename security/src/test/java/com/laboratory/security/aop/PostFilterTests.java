a9035d9f3e83151e5c1e439562fa8db6d25fd82d
Yongbum Kim <dt10002@naver.com>
2025-12-28 23:19:54 +0900

    test: PostFilter 테스트

---
+++ security/src/test/java/com/laboratory/security/aop/PostFilterTests.java
@@ -0,0 +1,52 @@
+package com.laboratory.security.aop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.laboratory.security.TestUserContext;
+import com.laboratory.security.domain.model.Employee;
+import com.laboratory.security.repository.ManagementRepository;
+import com.laboratory.security.service.V2ManagementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+
+@SpringBootTest
+class PostFilterTests extends TestUserContext {
+
+  @SpyBean
+  ManagementRepository repository;
+
+  @Autowired
+  V2ManagementService sut;
+
+  @BeforeEach
+  void setUp() {
+    SecurityContextHolder.clearContext();
+    repository.init();
+  }
+
+  @DisplayName("@PostFilter - 직원 본인이 자신의 직원들 조회 성공")
+  @Test
+  void junhyunny_getEmployees_success() {
+
+    // given
+    SecurityContextHolder.setContext(new SecurityContextImpl(junhyunnyAuthentication));
+
+    // when
+    var result = sut.getMyEmployees();
+
+    // then
+    assertEquals(3, result.size());
+    assertEquals(new Employee("M0001", "S0001", "junhyunny"), result.get("M0001"));
+    assertEquals(new Employee("E0001", "M0001", "jua"), result.get("E0001"));
+    verify(repository, times(1)).getAllEmployees();
+  }
+
+}
