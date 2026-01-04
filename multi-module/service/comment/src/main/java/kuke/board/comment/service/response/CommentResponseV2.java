4a1e909f548011b1ebb7e3f24949b62f02ccb034
Yongbum Kim <dt10002@naver.com>
2026-01-05 01:21:16 +0900

    feat: 무한 대댓글 요청 및 응답 객체 구현

---
+++ multi-module/service/comment/src/main/java/kuke/board/comment/service/response/CommentResponseV2.java
@@ -0,0 +1,31 @@
+package kuke.board.comment.service.response;
+
+import java.time.LocalDateTime;
+import kuke.board.comment.entity.CommentV2;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class CommentResponseV2 {
+
+  private Long commentId;
+  private String content;
+  private Long articleId;
+  private Long writerId;
+  private Boolean deleted;
+  private String path;
+  private LocalDateTime createdAt;
+
+  public static CommentResponseV2 from(CommentV2 comment) {
+    CommentResponseV2 response = new CommentResponseV2();
+    response.commentId = comment.getCommentId();
+    response.content = comment.getContent();
+    response.articleId = comment.getArticleId();
+    response.writerId = comment.getWriterId();
+    response.deleted = comment.getDeleted();
+    response.path = comment.getCommentPath().getPath();
+    response.createdAt = comment.getCreatedAt();
+    return response;
+  }
+}
