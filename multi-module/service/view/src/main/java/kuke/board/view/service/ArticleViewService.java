8992fd33ae9deef89b8a6e7284e3a1a274911c4b
Yongbum Kim <dt10002@naver.com>
2026-01-06 16:50:46 +0900

    feat: 분산락을 활용한 조회수 어뷰징 방지 기능 구현

--- multi-module/service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
+++ multi-module/service/view/src/main/java/kuke/board/view/service/ArticleViewService.java
@@ -1,6 +1,8 @@
 package kuke.board.view.service;
 
+import java.time.Duration;
 import kuke.board.view.repository.ArticleViewCountRepository;
+import kuke.board.view.repository.ArticleViewDistributedLockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,16 @@ public class ArticleViewService {
 
   private final ArticleViewCountRepository articleViewCountRepository;
   private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+  private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
   private static final int BACK_UP_BATCH_SIZE = 100;
+  private static final Duration TTL = Duration.ofMinutes(10);
 
   public Long increase(Long articleId, Long userId) {
+    if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+      return articleViewCountRepository.read(articleId);
+    }
+
     Long count = articleViewCountRepository.increase(articleId);
     if (count % BACK_UP_BATCH_SIZE == 0) {
       articleViewCountBackUpProcessor.backUp(articleId, count);
