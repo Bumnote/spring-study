b3bbdad8471f2f88e638be36f85edad9469c8720
Yongbum Kim <dt10002@naver.com>
2026-01-10 16:28:28 +0900

    feat: 인기글을 위한 게시글 조회수 관련 Repository 구현

---
+++ multi-module/service/hot-article/src/main/java/kuke/board/hotarticle/repository/ArticleViewCountRepository.java
@@ -0,0 +1,29 @@
+package kuke.board.hotarticle.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ArticleViewCountRepository {
+
+  private final StringRedisTemplate redisTemplate;
+
+  // hot-article::article::{articleId}::view-count
+  private static final String KEY_FORMAT = "hot-article::article::%s::view-count";
+
+  public void createOrUpdate(Long articleId, Long viewCount) {
+    redisTemplate.opsForValue().set(generateKey(articleId), String.valueOf(viewCount));
+  }
+
+  public Long read(Long articleId) {
+    String result = redisTemplate.opsForValue().get(generateKey(articleId));
+    return result == null ? 0L : Long.valueOf(result);
+  }
+
+  private String generateKey(Long articleId) {
+    return KEY_FORMAT.formatted(articleId);
+  }
+
+}
