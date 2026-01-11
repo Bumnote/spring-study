c1d6a0d4bdee2a5520a0614b736c68ab708c6921
Yongbum Kim <dt10002@naver.com>
2026-01-11 21:14:37 +0900

    test: 인기글 TTL을 위한 시간 계산 클래스 테스트

---
+++ multi-module/service/hot-article/src/test/java/kuke/board/hotarticle/utils/TimeCalculatorUtilsTest.java
@@ -0,0 +1,16 @@
+package kuke.board.hotarticle.utils;
+
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+
+class TimeCalculatorUtilsTest {
+
+  @Test
+  void test() {
+
+    // given
+    Duration duration = TimeCalculatorUtils.calculateDurationToMidnight();
+
+    System.out.println("duration.getSeconds() / 60 = " + duration.getSeconds() / 60);
+  }
+}
