95769bf3fd1f47cdca0735adce21c964d4c183cd
Yongbum Kim <dt10002@naver.com>
2026-03-20 00:15:41 +0900

    feat: model generate

--- spring-ai/src/main/java/com/laboratory/springai/domain/ai/OpenAiService.java
+++ spring-ai/src/main/java/com/laboratory/springai/domain/ai/OpenAiService.java
@@ -2,6 +2,10 @@ package com.laboratory.springai.domain.ai;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
@@ -14,13 +18,17 @@ import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
 import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
 import org.springframework.ai.openai.OpenAiEmbeddingOptions;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -115,5 +123,44 @@ public class OpenAiService {
     return response.getResults().stream()
         .map(image -> image.getOutput().getUrl())
         .toList();
+  }
+
+  // TTS
+  public byte[] tts(String text) {
+
+    // 옵션
+    OpenAiAudioSpeechOptions speechOptions = OpenAiAudioSpeechOptions.builder()
+        .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+        .speed(1.0)
+        .model(OpenAiAudioApi.TtsModel.TTS_1.value)
+        .build();
+
+    // 프롬프트
+    TextToSpeechPrompt prompt = new TextToSpeechPrompt(text, speechOptions);
+
+    // 요청 및 응답
+    TextToSpeechResponse response = openAiAudioSpeechModel.call(prompt);
+    return response.getResult().getOutput();
+  }
+
+  // STT
+  public String stt(Resource audioFile) {
+
+    // 옵션
+    OpenAiAudioApi.TranscriptResponseFormat responseFormat = OpenAiAudioApi.TranscriptResponseFormat.VTT;
+    OpenAiAudioTranscriptionOptions transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+        .language("ko") // 인식할 언어
+        .prompt("Ask not this, but ask that") // 음성 인식 전 참고할 텍스트 프롬프트
+        .temperature(0f)
+        .model(OpenAiAudioApi.TtsModel.TTS_1.value)
+        .responseFormat(responseFormat) // 결과 타입 지정 VTT 자막형식
+        .build();
+
+    // 프롬프트
+    AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(audioFile, transcriptionOptions);
+
+    // 요청 및 응답
+    AudioTranscriptionResponse response = openAiAudioTranscriptionModel.call(prompt);
+    return response.getResult().getOutput();
   }
 }
